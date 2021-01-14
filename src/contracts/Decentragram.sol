13bb8ba9439cc114d3b9b2262ab3b7f78eb69fd2
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-01-14 11:58:04 +0900

    created smart contract with mapping plus test

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,4 +2,17 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
   string public name = "Decentragram";
+  mapping(uint => Image) public images;
+
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  function uploadImage() public {
+    images[1] = Image(1, 'abc123', 'hello world', 0, address(0x0));
+  }
 }
