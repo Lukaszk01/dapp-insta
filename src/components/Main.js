8443b86a8923c4e819c17b5e33a070f4a31382cf
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-01-26 21:36:45 +0900

    form

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,23 +11,27 @@ class Main extends Component {
             <div className="content mr-auto ml-auto">
               <p>&nbsp;</p>
               <h2>Share Image</h2>
-              
+              <form onSubmit={(event) => {
+                event.preventDefault()
+                const description = this.imageDescription.value
+                this.props.uploadImage(description)
+              }} >
                 <input type='file' accept=".jpg, .jpeg, .png, .bmp, .gif" onChange={this.props.captureFile} />
-                  <div className="form-group mr-sm-2">
-                    <br></br>
-                      <input
-                        id="imageDescription"
-                        type="text"
-                        ref={(input) => { this.imageDescription = input }}
-                        className="form-control"
-                        placeholder="Image description..."
-                        required />
-                  </div>
+                <div className="form-group mr-sm-2">
+                  <br></br>
+                  <input
+                    id="imageDescription"
+                    type="text"
+                    ref={(input) => { this.imageDescription = input }}
+                    className="form-control"
+                    placeholder="Image description..."
+                    required />
+                </div>
                 <button type="submit" className="btn btn-primary btn-block btn-lg">Upload!</button>
               </form>
               <p>&nbsp;</p>
-              { this.props.images.map((image, key) => {
-                return(
+              {this.props.images.map((image, key) => {
+                return (
                   <div className="card mb-4" key={key} >
                     <div className="card-header">
                       <img
@@ -40,7 +44,7 @@ class Main extends Component {
                     </div>
                     <ul id="imageList" className="list-group list-group-flush">
                       <li className="list-group-item">
-                        <p class="text-center"><img src={`https://ipfs.infura.io/ipfs/${image.hash}`} style={{ maxWidth: '420px'}}/></p>
+                        <p class="text-center"><img src={`https://ipfs.infura.io/ipfs/${image.hash}`} style={{ maxWidth: '420px' }} /></p>
                         <p>{image.description}</p>
                       </li>
                       <li key={key} className="list-group-item py-2">
